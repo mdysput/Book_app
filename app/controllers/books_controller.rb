14a4298198eff9702a57dad628e4790d81201627
Michal <michal.dysput@gmail.com>
2019-05-09 11:04:47 +0200

    Categories added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,12 @@ class BooksController < ApplicationController
   before_action :find_book, only: [:show, :edit, :update, :destroy]
 
   def index
-    @books = Book.all.order("created_at DESC")
+    if params[:category].blank?
+      @books = Book.all.order("created_at DESC")
+    else
+      @category_id= Category.find_by(name: params[:category]).id
+      @books = Book.where(:category_id => @category_id).order("created_at DESC")
+    end
   end
 
   def show
@@ -27,7 +32,9 @@ class BooksController < ApplicationController
   def edit
   end
 
+
   def update
+    @book.category_id= params[:category_id]
     if @book.update(book_params)
       redirect_to book_path(@book)
     else
@@ -42,7 +49,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :description, :author)
+    params.require(:book).permit(:title, :description, :author, :category_id)
   end
 
   def find_book
